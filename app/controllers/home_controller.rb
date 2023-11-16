7ce940d11be44d7804826a4cc32d3f36742651e2
desinghrajan <desinghrajan@gmail.com>
2023-11-16 17:10:59 +0530

    Adding devise for user authentication

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,11 @@
+class HomeController < ApplicationController
+  def index
+    # @user = current_user
+  end
+
+  def about
+  end
+
+  def features
+  end
+end
---
+++ test/controllers/home_controller_test.rb
@@ -0,0 +1,18 @@
+require "test_helper"
+
+class HomeControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get home_index_url
+    assert_response :success
+  end
+
+  test "should get about" do
+    get home_about_url
+    assert_response :success
+  end
+
+  test "should get features" do
+    get home_features_url
+    assert_response :success
+  end
+end
